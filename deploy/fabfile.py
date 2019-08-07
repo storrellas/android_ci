2b64354c4bd24f766eba70929ef92bc0e9b45e2f
storrellas <storrellas@gmail.com>
2019-08-07 06:29:11 +0000

    ENH: Dockerfile

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -227,8 +227,8 @@ def deployci(c):
             #c.run('cp -rv ' + docker_folder + '/.env.template ' + docker_folder + '/.env', echo=True)
 
             # Upload configuration
-            # print('../{}/.env.template'.format(docker_folder))
-            # print(config['remote_workspace'] + '/' + repo_ci_folder + docker_folder)
+            print('../{}/.env.template'.format(docker_folder))
+            print(config['remote_workspace'] + '/' + repo_ci_folder + docker_folder)
             c.put('../{}/.env.template'.format(docker_folder), remote=config['remote_workspace'] + '/' + repo_ci_folder + docker_folder)
             c.run('cp -rv {}/.env.template {}/.env'.format(docker_folder, docker_folder))
 
