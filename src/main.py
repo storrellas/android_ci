194f59e41b5b95de308e06ac6ffb8b68a0ca560a
storrellas <storrellas@gmail.com>
2019-08-12 21:39:55 +0000

    ENH: Docker starts from ubuntu

--- src/main.py
+++ src/main.py
@@ -23,6 +23,20 @@ def run_container_volume():
   # It comes as bytes -> Silly thing
   print(logs.decode())
 
+
+
+def run_container_gradle_syslog():
+  command="./gradlew build"
+  container = client.containers.run(image="android_ci", 
+                                command=command, 
+                                log_config=LogConfig(type=LogConfig.types.SYSLOG, config={}),
+                                working_dir="/home/gradle/project/",
+                                volumes={'/home/vagrant/workspace/we_are_nutrition-android': {'bind': '/home/gradle/project/', 'mode': 'rw'}},
+                                extra_hosts={"nexus.nespresso.com":"192.168.216.107"}, detach=True)
+  # It comes as bytes -> Silly thing
+  # logs = container.logs()
+  # print(logs.decode())
+
 def run_container_gradle():
   command="./gradlew " \
             "-Dhttp.proxyHost=barc.proxy.corp.sopra -Dhttp.proxyPort=8080 " \
@@ -38,44 +52,23 @@ def run_container_gradle():
   for line in container.logs(stream=True):
     print(line.strip())
 
-
-def run_container_gradle_syslog():
-  command="./gradlew build"
+def run_container_gradle_noproxy():
+  command="./gradlew build --debug"
   container = client.containers.run(image="android_ci", 
                                 command=command, 
-                                log_config=LogConfig(type=LogConfig.types.SYSLOG, config={}),
+                                #log_config=LogConfig(type=LogConfig.types.SYSLOG, config={}),
                                 working_dir="/home/gradle/project/",
                                 volumes={'/home/vagrant/workspace/we_are_nutrition-android': {'bind': '/home/gradle/project/', 'mode': 'rw'}},
                                 extra_hosts={"nexus.nespresso.com":"192.168.216.107"}, detach=True)
-  # It comes as bytes -> Silly thing
-  # logs = container.logs()
-  # print(logs.decode())
+  for line in container.logs(stream=True):
+    print(line.strip())
 
 
 if __name__ == "__main__":
   client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-  #print(client.containers.run("android_ci", "gradle"))
-#   container = client.containers.create(
-#     image='android_ci',
-#     stdin_open=True,
-#     tty=True,
-#     command='ls -la',
-#     volumes=['/home/storrellas/workspace/we_are_nutrition-android'],
-#     host_config=client.create_host_config(binds={
-#         '/home/storrellas/workspace/we_are_nutrition-android': {
-#             'bind': '/home/gradle/project/',
-#             'mode': 'rw',
-#         }
-#     })
-# )
-# print(client.start(container))
-  # print("Running container")
-  # container = client.containers.run(image="android_ci", command="echo hello world", detach=True)
-  # print("Grab logs")
-  # logs = container.logs()
-  # print(logs)
 
   #run_container_volume()
-  run_container_gradle()
+  #run_container_gradle()
+  run_container_gradle_noproxy()
 
   
